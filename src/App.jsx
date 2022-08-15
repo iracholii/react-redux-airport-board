91292777dfc210599b6959cd15c2f347cfbbf873
iracholii <ira.cholii.m@gmail.com>
2022-08-15 16:57:52 +0300

    updating

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import store from './store';
 import AirportBoard from './airportBoard/components/airportBoard/AirportBoard';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <AirportBoard />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   );
 };
